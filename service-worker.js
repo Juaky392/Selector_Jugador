787e9d7b737ff49e14326cb86cb8eb00617f2bea
Joaquin Lledo Muñoz <joaquinlledo43@gmail.com>
2025-09-21 11:39:39 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // service-worker.js
-const VERSION = "v5";                     // ⬅️ súbelo cuando hagas cambios
+const VERSION = "v6";                     // ⬅️ súbelo cuando hagas cambios
 const CACHE_NAME = `dedos-cache-${VERSION}`;
 const CORE = [
   "./",
